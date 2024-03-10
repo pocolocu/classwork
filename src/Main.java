99ef548de086d06c82736f5ba9cf88371dd3c299
pocolocu <tima.burlachenko@gmail.com>
2024-03-10 11:38:58 +0200

    add

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+//            System.out.println(arr[i]);
+        }
+        System.out.println(sum);
+    }
+}
